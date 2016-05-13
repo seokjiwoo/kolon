45a18dac391ce000ed0695d120d4219d475fcdea
유혜수 <hsyoo@vinyl-x.com>
2016-05-13 18:47:09 +0900

    main

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 jQuery(function($) {
+	$('.cardWrap').isotope({
+		itemSelector: '.cardWrap > li',
+		masonry: {
+			columnWidth: 100,
+			gutter: 1
+		}
+	});
 	$('#btnLnb').click(function(e){
 		e.preventDefault();
 		$('#lnb').animate({'left':'0'}, 500);
@@ -15,12 +22,20 @@ jQuery(function($) {
 		e.preventDefault();
 		$('#myMenu').slideToggle();
 	});
-	$('.cardWrap').isotope({
-		itemSelector: '.cardWrap li',
-		masonry: {
-			columnWidth: 100,
-			gutter: 1
+	$('.cardCollect label').click(function(){
+		if ($(this).siblings('input').val() != ':checked'){			
+			$(this).parent().addClass('on');
+			$(this).parent().siblings().removeClass('on');
 		}
+	})
+	$('a.cardCon').hover(function(){
+    	$(this).parent().stop().animate({'background-size':'110%'}, 300);
+    	$(this).find('.hoverCon').stop().slideDown(400);
+    	$(this).find('.hoverOff').stop().slideUp(100);
+    }, function(){
+    	$(this).parent().stop().animate({'background-size':'100%'}, 300);
+    	$(this).find('.hoverCon').stop().slideUp(100)
+    	$(this).find('.hoverOff').stop().slideDown(400);
 	});
 
 });
