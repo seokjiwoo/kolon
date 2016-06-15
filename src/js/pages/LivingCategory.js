4d86c5562d06b112e1417ac8253a1fc1099a6af2
hkroh <hkroh@vinyl-x.com>
2016-06-15 15:51:14 +0900

    [ living/category.html / popDetailedMore.html ] 1. rangeSlider 적용( 가격 범위 설정 ) / 2. data-attribute 옵션 설정 적용 / 3.마크업 구조를 임의로 구성하여서 마크업 작업자가 검토 필요 [ LivingCategory.js ] LivingCategory 페이지 UI JS 구성, rangeSlider 구현 [ jquery-ui-slider.css ] jquery-ui slider css 추가

--- src/js/pages/LivingCategory.js
+++ src/js/pages/LivingCategory.js
@@ -7,9 +7,11 @@ module.exports = function() {
 	$ = win.jQuery,
 	doc = document,
 	DEBUG = require('../utils/Console.js'),
+	util = require('../utils/Util.js'),
 	FILE_NAME = 'LivingCategory.js';
 
 	var opts = {
+		wrap : '.js-slider-wrap',
 		colorbox : {
 			target : '#colorbox',
 			event : {
@@ -19,7 +21,23 @@ module.exports = function() {
 			}
 		},
 		rangeSlider : {
-			target : '.js-range-slider'
+			target : '.js-slider',
+			opts : {
+				range : true,
+				min : 0,
+				max : 500,
+				step : 1,
+				values : [ 75, 300 ],
+				create : $.noop,
+				slide : $.noop,
+				stop : $.noop,
+				currency : '만원'
+			}
+		},
+		info : {
+			values : '.js-slider-values',
+			leftValue : '.js-slider-lv',
+			rightValue : '.js-slider-rv'
 		},
 		cssClass : {
 			categoryPop : 'categoryPop'
@@ -90,31 +108,67 @@ module.exports = function() {
 	function setRangeSlider() {
 		DEBUG.log(FILE_NAME, 'setRangeSlider');
 		
-		self.slider = self.colorbox.find(opts.rangeSlider.target);
-		self.info = self.colorbox.find('.graphData');
+		self.wrap = self.colorbox.find(opts.wrap);
+		self.slider = self.wrap.find(opts.rangeSlider.target);
+		self.infoValues = self.wrap.find(opts.info.values);
+		self.infoLeft = self.wrap.find(opts.info.leftValue);
+		self.infoRight = self.wrap.find(opts.info.rightValue);
 		
 		if (self.slider.slider('instance')) {
 			return;
 		}
 
-		self.slider.slider({
-			range : true,
-			min : 0,
-			max : 500,
-			values : [ 75, 300 ],
-			create : onSliderCreate,
-			slide : onSliderChange
-		});
+		var sliderOpts = {},
+		dataOpts = self.slider.data('slider-opt');
+
+		if (dataOpts) {
+			sliderOpts = $.extend(true, opts.rangeSlider.opts, dataOpts);
+		}
+
+		if (sliderOpts.create === $.noop) {
+			sliderOpts.create = onSliderCreate;
+		}
+
+		if (sliderOpts.slide === $.noop) {
+			sliderOpts.slide = onSliderChange;	
+		}
+
+		if (sliderOpts.stop === $.noop) {
+			sliderOpts.stop = setValuesPos;
+		}
+
+		self.currency = sliderOpts.currency;
+
+		DEBUG.log(FILE_NAME, 'setRangeSlider data-slide-opt : ', self.slider, self.slider.data('slider-opt'), sliderOpts);
+		self.slider.slider(sliderOpts);
 	}
 
 	function onSliderCreate(e, ui) {
-		DEBUG.log(FILE_NAME, 'onSliderCreate');
+		DEBUG.log(FILE_NAME, 'onSliderCreate', e, ui);
+
+		var inst = self.slider.slider('instance'),
+		handles = inst.handles;
+
+		self.range = $(inst.range);
+		self.handleLeft = $(handles[0]);
+		self.handleRight = $(handles[1]);
+
+		self.handleLeft.addClass('leftBtn');
+		self.handleRight.addClass('rightBtn');
+
 		onSliderChange(e, self.slider.slider('option'));
 	}
 
 	function onSliderChange(e, ui) {
-		DEBUG.log(FILE_NAME, 'onSliderChange', ui.values[0] + ' ~ ' + ui.values[1]);
-		self.info.text(ui.values[0] + ' ~ ' + ui.values[1]);
+		DEBUG.log(FILE_NAME, 'onSliderChange', ui, ui.values[0] + ' ~ ' + ui.values[1]);
+		var leftValue = util.currencyFormat(ui.values[0]),
+		rightValue = util.currencyFormat(ui.values[1]);
+
+		self.infoValues.text(leftValue + ' ~ ' + rightValue);
+		self.infoLeft.text(leftValue + self.currency);
+		self.infoRight.text(rightValue + self.currency);
+
+		setValuesPos();
 	}
 
 	function destoryRangeSlider() {
@@ -124,6 +178,16 @@ module.exports = function() {
 		if (self.slider.slider('instance')) {
 			self.slider.slider('destroy');
 		}
+	}
+
+	function setValuesPos() {
+		var widthPer = (self.range.width() / self.range.offsetParent().width()) * 100,
+		leftPer = (self.range.position().left / self.slider.outerWidth())  * 100,
+		posPer = ((widthPer/2) + leftPer);
+
+		self.infoValues.css({
+			'left' : posPer + '%'
+		});
 	}
 
 };
