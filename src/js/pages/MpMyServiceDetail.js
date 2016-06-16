d852f82c14c184536e7cb3e797cf39132da56ead
hkroh <hkroh@vinyl-x.com>
2016-06-16 13:56:00 +0900

    [ myPage/myServiceDetail.html ] 1. graph 기본 구성 / 2. 도넛 라운딩 svg & d3.js 적용 / 3. 마크업 구조 -  테스트로 임시 구성

--- src/js/pages/MpMyServiceDetail.js
+++ src/js/pages/MpMyServiceDetail.js
@@ -24,8 +24,27 @@ module.exports = function() {
 		Super.init();
 
 		DEBUG.log(FILE_NAME, 'init');
-		
-		var diff = util.diffDay({ startDate : '2016.04.15', endDate : '2016.04.30'});
+
+		var graph = $('.js-graph'),
+		opts = graph.data('graph-opt'),
+		diff = util.diffDay({
+			startDate : (opts.startDate === 'today' ? new Date() : opts.startDate),
+			endDate : opts.endDate
+		});
+
+		DEBUG.log(FILE_NAME, 'init > graph-opt', opts);
+
+		if (diff.diffDay && diff.diffDay > 0) {
+			graph.find('.js-graph-progress').text('시공진행중');
+			graph.find('.js-graph-dday').text('D-' + diff.diffDay);
+		} else {
+			graph.find('.js-graph-progress').text('----- 문구 ----');
+			if (diff.diffDay === 0) {
+				graph.find('.js-graph-dday').text('D-Day');
+			} else {
+				graph.find('.js-graph-dday').text(diff.diffDay);
+			}			
+		}
 
 		DEBUG.log(FILE_NAME, 'diffDay', diff.diffDay);
 	}
