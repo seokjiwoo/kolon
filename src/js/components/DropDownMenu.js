58dd114d7348166fe47c91d946b520a159b98616
hkroh <hkroh@vinyl-x.com>
2016-06-24 16:13:03 +0900

    [ DropDownMenu ] 1. 기능 구성 / 2. Page.js 상에서 공통 적용 설정

--- src/js/components/DropDownMenu.js
+++ src/js/components/DropDownMenu.js
@@ -7,7 +7,6 @@ function DropDownMenu() {
 	var win = window,
 	$ = win.jQuery,
 	debug = require('../utils/Console.js'),
-	util = require('../utils/Util.js'),
 	fileName = 'compoents/DropDownMenu.js';
 
 	var opts = {
@@ -19,7 +18,8 @@ function DropDownMenu() {
 			dropOn : 'dropOn'
 		},
 		dataAttr : {
-			prevent : 'prevent'
+			prevent : 'prevent',
+			allChecker : 'all-checker'
 		}
 	};
 
@@ -67,8 +67,12 @@ function DropDownMenu() {
 				.on(self.EVENT.DESTROY, $.proxy(destroy, self));
 
 		$.each(self.drops, function() {
-			$(this).find('a').eq(0).on('click', onDropMenuToggle);
-			$(this).find('a:not(:first)').on('click', onDropMenuClick);
+			$(this).find('a').eq(0).on('click', $.proxy(onDropMenuToggle, self));
+			$(this).find('a:not(:first)').on('click', $.proxy(onDropMenuClick, self));
+		});
+
+		$.each(self.dropCheckMenus, function() {
+			$(this).find('input').on('change', $.proxy(onDropCheckChange, self));
 		});
 	}
 
@@ -80,8 +84,9 @@ function DropDownMenu() {
 				.off(self.EVENT.DESTROY, $.proxy(destroy, self));
 
 		$.each(self.drops, function() {
-			$(this).find('a').eq(0).off('click', onDropMenuToggle);
-			$(this).find('a:not(:first)').off('click', onDropMenuClick);
+			$(this).find('a').eq(0).off('click', $.proxy(onDropMenuToggle, self));
+			$(this).find('a:not(:first)').off('click', $.proxy(onDropMenuClick, self));
+			$(this).off('focusoutside mousedownoutside', $.proxy(onDropMenuClose, self));
 		});
 	}
 
@@ -95,22 +100,92 @@ function DropDownMenu() {
 		}
 
 		dropMenu.toggleClass(self.opts.cssClass.dropOn);
+
+		if (dropMenu.hasClass(self.opts.cssClass.dropOn)) {
+			dropMenu.one('focusoutside mousedownoutside', $.proxy(onDropMenuClose, self));
+		}
 	}
 
 	function onDropMenuClick(e) {
 		e.preventDefault();
 
 		var target = $(e.currentTarget),
-		dropMenu = target.closest(self.opts.dropMenus);
+		dropMenu = target.closest(self.opts.dropMenus),
+		menus = dropMenu.find('a');
 
-		dropMenu.find('a.' + self.opts.cssClass.active)
-				.removeClass(self.opts.cssClass.active);
-
+		menus.filter('.' + self.opts.cssClass.active).removeClass(self.opts.cssClass.active);
 		target.addClass(self.opts.cssClass.active);
+		menus.eq(0).text(target.text());
 
 		dropMenu.removeClass(self.opts.cssClass.dropOn)
 				.val([target.data('value')])
-				.trigger(self.EVENT.CHANGE, {value : dropMenu.val()});
+				.trigger(self.EVENT.CHANGE, {menu: dropMenu, values : dropMenu.val()});
+				
+		$('body').trigger(self.EVENT.CHANGE, {menu: dropMenu, values : dropMenu.val()});
+
+		dropMenu.off('focusoutside mousedownoutside', $.proxy(onDropMenuClose, self));
+	}
+
+	function onDropCheckChange(e) {
+		var target = $(e.currentTarget),
+		dropCheckMenu = target.closest(self.opts.dropCheckMenus),
+		hasAllChecker = dropCheckMenu.find('[data-' + self.opts.dataAttr.allChecker + ']'),
+		checks = dropCheckMenu.find('input'),
+		labels = dropCheckMenu.find('label'),
+		idx = checks.index(target),
+		isChecked = target.prop('checked');
+
+		// 전체선택 토글 처리
+		if ((hasAllChecker.size() && target.data(self.opts.dataAttr.allChecker)) || (!hasAllChecker.size() && idx === 0)) {
+			checks.prop('checked', isChecked);
+			if (isChecked) {
+				labels.addClass(self.opts.cssClass.active);
+			} else {
+				labels.removeClass(self.opts.cssClass.active);
+			}
+
+			triggerDropCheckValues(dropCheckMenu, checks);
+			return;
+		}
+
+		// input 요소 선택에 따른 '전체선택' 활성화처리
+		if (target.prop('checked')) {
+			if (checks.size() - checks.filter(':checked').size() === 1) {
+				if (hasAllChecker.size()) {
+					hasAllChecker.prop('checked', true);
+					hasAllChecker.siblings('label').addClass(self.opts.cssClass.active);
+				} else {
+					checks.eq(0).prop('checked', true);
+					labels.eq(0).addClass(self.opts.cssClass.active);
+				}
+			}
+		} else {
+			if (hasAllChecker.size()) {
+				hasAllChecker.prop('checked', false);
+				hasAllChecker.siblings('label').removeClass(self.opts.cssClass.active);
+			} else {
+				checks.eq(0).prop('checked', false);
+				labels.eq(0).removeClass(self.opts.cssClass.active);
+			}
+		}
+
+		triggerDropCheckValues(dropCheckMenu, checks);
+	}
+
+	function triggerDropCheckValues(dropCheckMenu, checks) {
+		var values = [];
+		$.each(checks.filter(':checked'), function() {
+			values.push($(this).attr('name'));
+		});
+
+		$('body').trigger(self.EVENT.CHANGE, {menu: dropCheckMenu, values : values});
+		dropCheckMenu.val(values)
+					.trigger(self.EVENT.CHANGE, {menu: dropCheckMenu, values : values});
+	}
+
+	function onDropMenuClose(e) {
+		var target = $(e.currentTarget);
+		target.removeClass(self.opts.cssClass.dropOn);
 	}
 
 	function destroy() {
