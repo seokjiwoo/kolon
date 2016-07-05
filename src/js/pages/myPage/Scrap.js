5f5094ecc98fe6da0f987bf237b557c4962c4215
hkroh <hkroh@vinyl-x.com>
2016-07-05 18:48:18 +0900

    [ scrap ] api controller 연동 설정

--- src/js/pages/myPage/Scrap.js
+++ src/js/pages/myPage/Scrap.js
@@ -16,6 +16,7 @@ module.exports = function() {
 	events = require('../../events/events'),
 	SCRAP_EVENT = events.SCRAP,
 	COLORBOX_EVENT = events.COLOR_BOX,
+	ISOTOPE_EVENT = events.ISOTOPE,
 	callerObj = {
 		/**
 		 * 초기화
@@ -23,6 +24,44 @@ module.exports = function() {
 		init: init
 	},
 	self;
+
+	var opts = {
+		container : '.container',
+		categoryMenu : '.tabType01 a',
+		cardWrap : '#cardWrap',
+		templates : {
+			wrap : '#scrapAll',
+			all : '#scrap-all-templates',
+			images : '#scrap-images-templates'
+		},
+		colorbox : '#colorbox',
+		editFolder : {
+			wrap : 'scrapFolderEdit',
+			inp : '.js-scrapFolderEdit-inp',
+			delete : '.js-scrapFolderEdit-delete',
+			submit : '.js-scrapFolderEdit-submit'
+		},
+		newFolder : {
+			wrap : 'scrapFolderNew',
+			inp : '.js-scrapFolderNew-inp',
+			submit : '.js-scrapFolderNew-submit'
+		},
+		removeFolder : {
+			wrap : 'scrapFolderRemove',
+			submit : '.js-scrapFolderRemove-submit'
+		},
+		scrap : {
+			wrap : 'scrapRemove',
+			submit : '.js-scrapFolderRemove-submit'
+		},
+		cssClass : {
+			isLoading : 'is-loading'
+		},
+		dataAttr : {
+			popBtn : '[data-user-class]',
+			scrapFolderInfo : '[data-scrapfolder-info]'
+		}
+	};
 	
 	return callerObj;
 	
@@ -31,15 +70,11 @@ module.exports = function() {
 		
 		debug.log(fileName, $, util, controller);
 
-		eventManager.on(COLORBOX_EVENT.WILD_CARD, function(e) {
-			debug.log(fileName, 'COLORBOX_EVENT.WILD_CARD', e.type);
-		});
-
 		self = callerObj;
+		self.opts = opts;
 
+		setElements();
 		setBindEvents();
-		activePageMenu();
-		// controller.scrapList();
 
 		// controller Method
 		// scrapList(attr) - 스크랩 카드 목록
@@ -52,177 +87,160 @@ module.exports = function() {
 		// deleteScrapFolder(folderNumber) - 스크랩 폴더 삭제
 	}
 
-	function activePageMenu() {
-		var category = util.getHashVar('category', win.location.href);
+	function setElements() {
+		self.container = $(self.opts.container);
+		self.templatesWrap = self.container.find(self.opts.templates.wrap);
+		self.categoryMenu = self.container.find(self.opts.categoryMenu);
+		self.colorbox = $(self.opts.colorbox);
+		self.selPopBtnInfo = {};
+	}
 
-		debug.log(fileName, 'activePageMenu', category);
 
+	function setBindEvents() {
+		$(controller).on(SCRAP_EVENT.WILD_CARD, onScrapControllerListener);
+		eventManager.on(COLORBOX_EVENT.WILD_CARD, onColorBoxAreaListener);
+
+		self.categoryMenu.on('click', onCategoryMenuClick);
+		self.templatesWrap.on('click', self.opts.dataAttr.popBtn, onWrapPopBtnClick);
+
+		$(win).on('hashchange', onHashChangeListener);
+		$(win).trigger('hashchange');
+	}
+
+	function onCategoryMenuClick(e) {
+		var target = $(e.currentTarget),
+		parent = target.closest('li'),
+		href = target.attr('href'),
+		category = util.getHashVar('category', href);
+
+		if (href && category) {
+			parent.addClass('on')
+					.siblings('li').removeClass('on');
+
+			debug.log(fileName, 'onCategoryMenuClick', category);
+		}
+	}
+
+	function onWrapPopBtnClick(e) {
+		var target = $(e.target),
+		info = target.closest(self.opts.dataAttr.scrapFolderInfo).data('scrapfolder-info');
+
+		self.selPopBtnInfo = {
+			target : target,
+			info : info
+		};
+
+		debug.log(fileName, 'onWrapPopBtnClick', self.selPopBtnInfo);
+	}
+
+	// hash change
+	function onHashChangeListener() {
+		var hashVars = util.getHashVar();
+
+		debug.log(fileName, 'onHashChangeListener', hashVars, hashVars.category);
+
+		activePageMenu(hashVars.category);
+
+		switch(hashVars.category) {
+			// 모든 스크랩
+			case 'all':
+				controller.scrapList('ALL');
+				break;
+			// 뉴폼/샵
+			case 'product':
+				controller.scrapList('PRODUCT');
+				break;
+			// 메거진
+			case 'magazine':
+				controller.scrapList('MAGAZINE');
+				break;
+			// 이미지
+			case 'images':
+				controller.scrapImageList(hashVars.folderNumber);
+				// if (hashVars.folderNumber) {
+				// 	controller.scrapImageList(hashVars.folderNumber);
+				// } else {
+				// 	controller.scrapList('IMAGE');
+				// }
+				break;
+			// 모든 스크랩
+			default:
+				controller.scrapList('ALL');
+				break;
+		}
+	}
+
+	// tab Active 처리
+	function activePageMenu(category) {
 		if (category) {
-			$('.tabType01 a[href=\'/myPage/scrap.html#category=' + util.getHashVar('category', win.location.href) + '\']').trigger('click');
+			$('.tabType01 a[href=\'/myPage/scrap.html#category=' + category + '\']').trigger('click');
 		} else {
 			$('.tabType01 a[href=\'/myPage/scrap.html#category=all\']').trigger('click');
 		}
 	}
 
-	function setBindEvents() {
-		$(controller).on(SCRAP_EVENT.WILD_CARD, onScrapControllerListener);
+	// Handlebars 마크업 템플릿 구성
+	function displayData(templates, data) {
+		var source = templates.html(),
+		template = win.Handlebars.compile(source),
+		insertElements = $(template(data));
 
-		$('.tabType01 a').on('click', function(e) {
-			var target = $(e.currentTarget),
-			parent = target.closest('li'),
-			href = target.attr('href'),
-			category = util.getHashVar('category', href);
+		eventManager.triggerHandler(ISOTOPE_EVENT.DESTROY);
+		self.templatesWrap.empty()
+							.addClass(self.opts.cssClass.isLoading)
+							.append(insertElements);
 
-			if (href && category) {
-				parent.addClass('on')
-						.siblings('li').removeClass('on');
-
-				debug.log(fileName, 'tab menu click', category);
-				controller.scrapList();
-			}
-		});
-		// $('.tabType01 a').on('click', funciton(e) {
-		// 	var target = $(e.target);
-		// });
+		self.templatesWrap.imagesLoaded()
+							.always(function() {
+								self.templatesWrap.removeClass(self.opts.cssClass.isLoading);
+								eventManager.triggerHandler(ISOTOPE_EVENT.REFRESH);
+								eventManager.triggerHandler(COLORBOX_EVENT.REFRESH);
+							});
 	}
 
-	function onScrapControllerListener(e, res) {
+	function onScrapControllerListener(e, status, response) {
 		var eventType = e.type,
-		status = res[0],
-		result = res[1];
-
-		var dummyData = {};
+		dummyData = {},
+		result = response;
 
 		switch(eventType) {
 			case SCRAP_EVENT.LIST:
-				dummyData = {
-					"folders": [
-						{
-							"folderName": "string",
-							"folderNumber": 0,
-							"scrapCount": 0,
-							"scrapImages": [
-								"string"
-							]
-						}
-					],
-					"scraps": [
-						{
-							"category": "string",
-							"imageUrl": "string",
-							"scrapCount": 0,
-							"scrapNumber": 0,
-							"tags": [
-								"string"
-							],
-							"magazine": {
-								"createDate": "string",
-								"subTitle": "string",
-								"title": "string"
-							}
-						},
-						{
-							"category": "string",
-							"imageUrl": "string",
-							"scrapCount": 0,
-							"scrapNumber": 0,
-							"tags": [
-								"string"
-							],
-							"product": {
-								"price": 0,
-								"productDesc": "string",
-								"productName": "string"
-							}
-						},
-						{
-							"category": "string",
-							"imageUrl": "string",
-							"scrapCount": 0,
-							"scrapNumber": 0,
-							"tags": [
-								"string"
-							],
-							"seller": {
-								"companyName": "string",
-								"sellerImageUrl": "string",
-								"sellerName": "string"
-							},
-						}
-					]
-				};
+				dummyData={"folders":[{"folderName":"folder0","folderNumber":0,"scrapCount":0,"scrapImages":["string"]},{"folderName":"folder1","folderNumber":1,"scrapCount":0,"scrapImages":["string"]}],"scraps":[{"category":"string","imageUrl":"string","scrapCount":0,"scrapNumber":0,"tags":["string"],"magazine":{"createDate":"string","subTitle":"string","title":"string"}},{"category":"string","imageUrl":"string","scrapCount":0,"scrapNumber":0,"tags":["string"],"product":{"price":0,"productDesc":"string","productName":"string"}},{"category":"string","imageUrl":"string","scrapCount":0,"scrapNumber":0,"tags":["string"],"seller":{"companyName":"string","sellerImageUrl":"string","sellerName":"string"},}]};
 
-				if (!result) {
-					result = dummyData;
+				/*
+				401	Unauthorized
+				403	Forbidden
+				404	Not Found
+				 */
+				switch(status) {
+					// case 200:
+					// 	break;
+					default:
+						result = dummyData;
+						break;
 				}
 
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
-				win.vinylX.demoScrapFolders.init($('body'), { dummyData : result });
-				eventManager.trigger(COLORBOX_EVENT.REFRESH);
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+				displayData($(self.opts.templates.all), result);
 				break;
 			case SCRAP_EVENT.IMAGE_LIST:
-				dummyData = {
-					"folders": [
-						{
-							"folderName": "string",
-							"folderNumber": 0,
-							"scrapCount": 0,
-							"scrapImages": [
-								"string"
-							]
-						}
-					],
-					"scraps": [
-						{
-							"category": "string",
-							"imageUrl": "string",
-							"scrapCount": 0,
-							"scrapNumber": 0,
-							"tags": [
-								"string"
-							],
-							"magazine": {
-								"createDate": "string",
-								"subTitle": "string",
-								"title": "string"
-							}
-						},
-						{
-							"category": "string",
-							"imageUrl": "string",
-							"scrapCount": 0,
-							"scrapNumber": 0,
-							"tags": [
-								"string"
-							],
-							"product": {
-								"price": 0,
-								"productDesc": "string",
-								"productName": "string"
-							}
-						},
-						{
-							"category": "string",
-							"imageUrl": "string",
-							"scrapCount": 0,
-							"scrapNumber": 0,
-							"tags": [
-								"string"
-							],
-							"seller": {
-								"companyName": "string",
-								"sellerImageUrl": "string",
-								"sellerName": "string"
-							},
-						}
-					]
-				};
+				dummyData = {"folders":[{"folderName":"폴더1","folderNumber":0,"recentImageTitle":"string","recentImageUrl":"string","scrapCount":0,"scrapImages":[]},{"folderName":"폴더2","folderNumber":0,"recentImageTitle":"string","recentImageUrl":"string","scrapCount":5,"scrapImages":[{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"}]},{"folderName":"폴더3","folderNumber":0,"recentImageTitle":"string","recentImageUrl":"string","scrapCount":10,"scrapImages":[{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"},{"imageTitle":"string","imageUrl":"../images/temp11.jpg"}]},{"folderName":"폴더3","folderNumber":0,"recentImageTitle":"string","recentImageUrl":"string","scrapCount":1,"scrapImages":[{"imageTitle":"string","imageUrl":"../images/temp11.jpg"}]}],"scraps":[{"category":"string","imageUrl":"string","magazine":{"createDate":"string","subTitle":"string","title":"string"},"product":{"price":0,"productDesc":"string","productName":"string"},"scrapCount":0,"scrapNumber":0,"seller":{"companyName":"string","sellerImageUrl":"string","sellerName":"string"},"tags":["string"]}]};
 
-				if (!result) {
-					result = dummyData;
+				/*
+				401	Unauthorized
+				403	Forbidden
+				404	Not Found
+				 */
+				switch(status) {
+					// case 200:
+					// 	break;
+					default:
+						result = dummyData;
+						break;
 				}
 
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+				displayData($(self.opts.templates.images), result);
 				break;
 			case SCRAP_EVENT.ADD_SCRAP:
 				/*
@@ -237,7 +255,15 @@ module.exports = function() {
 				403	Forbidden
 				404	Not Found
 				 */
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				switch(status) {
+					case 200:
+						break;
+					default:
+						break;
+				}
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+
+				testResult();
 				break;
 			case SCRAP_EVENT.EDIT_SCRAP:
 				/*
@@ -252,7 +278,15 @@ module.exports = function() {
 				403	Forbidden
 				404	Not Found
 				 */
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				switch(status) {
+					case 200:
+						break;
+					default:
+						break;
+				}
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+
+				testResult();
 				break;
 			case SCRAP_EVENT.DELETE_SCRAP:
 				/*
@@ -266,7 +300,15 @@ module.exports = function() {
 				401	Unauthorized
 				403	Forbidden
 				 */
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				switch(status) {
+					case 200:
+						break;
+					default:
+						break;
+				}
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+
+				testResult();
 				break;
 			case SCRAP_EVENT.ADD_SCRAP_FOLDER:
 				/*
@@ -281,7 +323,15 @@ module.exports = function() {
 				403	Forbidden
 				404	Not Found
 				 */
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				switch(status) {
+					case 200:
+						break;
+					default:
+						break;
+				}
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+
+				testResult();
 				break;
 			case SCRAP_EVENT.EDIT_SCRAP_FOLDER:
 				/*
@@ -296,7 +346,15 @@ module.exports = function() {
 				403	Forbidden
 				404	Not Found
 				 */
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				switch(status) {
+					case 200:
+						break;
+					default:
+						break;
+				}
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+
+				testResult();
 				break;
 			case SCRAP_EVENT.DELETE_SCRAP_FOLDER:
 				/*
@@ -310,7 +368,156 @@ module.exports = function() {
 				401	Unauthorized
 				403	Forbidden
 				 */
-				debug.log(fileName, 'onScrapControllerListener', eventType, res, status, result);
+				switch(status) {
+					case 200:
+						break;
+					default:
+						break;
+				}
+				debug.log(fileName, 'onScrapControllerListener', eventType, status, response);
+
+				testResult();
+				break;
+		}
+	}
+
+	function testResult() {
+		win.alert('임시처리 결과처리 - location.reload');
+		win.location.reload();
+	}
+
+	/**
+	 * 스크랩 폴더 삭제
+	 */
+	function onDeleteFolder(e) {
+		e.preventDefault();
+
+		var target = $(e.target),
+		// 스크랩 유무에 따른 - 문구 처리
+		message = (self.selPopBtnInfo.info.scrapCount) ? '해당 폴더를 삭제하실 경우에는\n폴더 안의 모든 이미지도 삭제됩니다!' : '해당 폴더를 삭제하시겠습니까?',
+		isAllow = win.confirm(message);
+
+		if (!isAllow) {
+			return;
+		}
+
+		debug.log(fileName, 'onDeleteFolder', target, self.selPopBtnInfo);
+		controller.deleteScrapFolder(self.selPopBtnInfo.info.folderNumber);
+	}
+
+
+	/**
+	 * 스크랩 폴더 수정
+	 */
+	function onEditFolder(e) {
+		e.preventDefault();
+
+		var target = $(e.target),
+		inp = self.colorbox.find(self.opts.editFolder.inp);
+
+		if (!inp.val() || inp.val() === ' ') {
+			win.alert('폴더명을 입력하세요.');
+			inp.focus();
+			return;
+		}
+		
+		if (inp.val() === self.selPopBtnInfo.info.folderName) {
+			debug.log(fileName, 'onEditFolder - 폴더명 동일', target, self.selPopBtnInfo, inp.val());
+		} else {
+			debug.log(fileName, 'onEditFolder', target, self.selPopBtnInfo, inp.val());	
+		}
+
+		controller.editScrapFolder(self.selPopBtnInfo.info.folderNumber, inp.val());
+	}
+
+	/**
+	 * 스크랩 폴더 등록
+	 */
+	function onNewFolder(e) {
+		e.preventDefault();
+
+		var target = $(e.target),
+		inp = self.colorbox.find(self.opts.newFolder.inp);
+
+		if (!inp.val() || inp.val() === ' ') {
+			win.alert('폴더명을 입력하세요.');
+			inp.focus();
+			return;
+		}
+
+		debug.log(fileName, 'onNewFolder', target, self.selPopBtnInfo, inp.val());
+		controller.addScrapFolder(inp.val());
+	}
+
+	/**
+	 * 스크랩 삭제
+	 */
+	function onDeleteScrap(e) {
+		e.preventDefault();
+
+		var target = $(e.target),
+		// 스크랩 유무에 따른 - 문구 처리
+		message = '해당 카드를 삭제하시겠습니까?',
+		isAllow = win.confirm(message);
+
+		if (!isAllow) {
+			return;
+		}
+
+		debug.log(fileName, 'onDeleteScrap', target, self.selPopBtnInfo);
+		controller.deleteScrap(self.selPopBtnInfo.info.scrapNumber);
+	}
+
+	function setColoboxFolder() {
+		if (self.colorbox.hasClass(self.opts.editFolder.wrap)) {
+			self.colorbox.find(self.opts.editFolder.inp).attr('placeholder', self.selPopBtnInfo.info.folderName);
+			self.colorbox.find(self.opts.editFolder.delete).on('click', onDeleteFolder);
+			self.colorbox.find(self.opts.editFolder.submit).on('click', onEditFolder);
+		}
+
+		if (self.colorbox.hasClass(self.opts.newFolder.wrap)) {
+			self.colorbox.find(self.opts.newFolder.submit).on('click', onNewFolder);
+		}
+
+		if (self.colorbox.hasClass(self.opts.removeFolder.wrap)) {
+			self.colorbox.find(self.opts.removeFolder.submit).on('click', onDeleteFolder);
+		}
+
+		if (self.colorbox.hasClass(self.opts.scrap.wrap)) {
+			self.colorbox.find(self.opts.scrap.submit).on('click', onDeleteScrap);
+		}
+
+		debug.log(fileName, 'setColoboxFolder');
+	}
+
+	function destroyColoboxFolder() {
+		if (self.colorbox.hasClass(self.opts.editFolder.wrap)) {
+			self.colorbox.find(self.opts.editFolder.delete).off('click', onDeleteFolder);
+			self.colorbox.find(self.opts.editFolder.submit).off('click', onEditFolder);
+		}
+
+		if (self.colorbox.hasClass(self.opts.newFolder.wrap)) {
+			self.colorbox.find(self.opts.newFolder.submit).off('click', onNewFolder);
+		}
+
+		if (self.colorbox.hasClass(self.opts.removeFolder.wrap)) {
+			self.colorbox.find(self.opts.removeFolder.submit).off('click', onDeleteFolder);
+		}
+
+		if (self.colorbox.hasClass(self.opts.scrap.wrap)) {
+			self.colorbox.find(self.opts.scrap.submit).off('click', onDeleteScrap);
+		}
+
+		debug.log(fileName, 'setColoboxFolder');
+	}
+
+	function onColorBoxAreaListener(e) {
+		switch(e.type) {
+			case COLORBOX_EVENT.COMPLETE:
+				setColoboxFolder();
+				break;
+			case COLORBOX_EVENT.CLEANUP:
+				destroyColoboxFolder();
 				break;
 		}
 	}
