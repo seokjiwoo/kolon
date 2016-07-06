5956d9df764138c826874b7045b44a3e5cebc593
Younbum Lee <eatenapple@gmail.com>
2016-07-06 16:16:22 +0900

    API 경로 수정

--- src/js/controller/APIController.js
+++ src/js/controller/APIController.js
@@ -2,8 +2,8 @@
 
 module.exports = function() {
 	var localFlag = (/localhost/).test(document.URL);
-	var API_URL = localFlag ? 'http://uppp.oneplat.co/' : '/';
-	var CDN_URL = localFlag ? 'http://cdn.oneplat.co/' : '/';
+	var API_URL = localFlag ? 'http://uppp.oneplat.co/' : document.URL.split('/')[0]+'//'+document.URL.split('/')[2]+'/';
+	var CDN_URL = localFlag ? 'http://cdn.oneplat.co/' : document.URL.split('/')[0]+'//'+document.URL.split('/')[2]+'/';
 	
 	$.ajaxSetup({
 		type: "POST"
