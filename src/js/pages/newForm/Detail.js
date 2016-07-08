51028df8b0bcb85f4733c3d1f41b0649ca3c1580
주중환 <hotbbar0719@gmail.com>
2016-07-08 18:51:40 +0900

    뉴폼 디테일 작업중

--- src/js/pages/newForm/Detail.js
+++ src/js/pages/newForm/Detail.js
@@ -24,5 +24,32 @@ module.exports = function() {
 	function init() {
 		Super.init();
 		debug.log(fileName, $, util);
+
+		$('.socialBtnOpen').click(function(e){
+			e.preventDefault();
+			if ($(this).parent('.socialBtn').hasClass('active')) {
+				$(this).parent('.socialBtn').removeClass('active');
+			} else {
+				$(this).parent('.socialBtn').removeClass('active').addClass('active');
+			}
+		});
+		$('.socialBtnClose').click(function(e){
+			e.preventDefault();
+			$(this).parent('.socialBtn').removeClass('active');
+		});
+
 	}
+
+	// function socialBtn() {
+	// 	$('.socialBtn').on('click', function() {
+	// 		if ($('.socialBtn').hasClass('active')) {
+	// 			$(this).removeClass('active');
+	// 		} else {
+	// 			$(this).removeClass('active').addClass('active');
+	// 		}
+	// 	};
+	// 	$('.socialBtn06 ').on('click', function() {
+	// 		$(this).parents('.socialBtn').removeClass('active');
+	// 	}
+	// }
 };
