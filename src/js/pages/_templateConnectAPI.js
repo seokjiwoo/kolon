837683bb7f08f24040967c59cd5a958a7247d6a9
hkroh <hkroh@vinyl-x.com>
2016-07-08 15:43:20 +0900

    [ _templateConnectAPI.js ] API 연결 템플릿 추가

---
+++ src/js/pages/_templateConnectAPI.js
@@ -0,0 +1,120 @@
+/* jshint node: true, strict: true */
+
+module.exports = function() {
+	'use strict';
+
+	var win = window,
+	$ = win.jQuery,
+	debug = require('../../utils/Console.js'),
+	util = require('../../utils/Util.js'),
+	fileName = '{{PATH}}/{{FILE_NAME}}.js';
+
+	var SuperClass = require('./Page.js'),
+	Super = SuperClass(),
+	controller = require('../../controller/{{CONTROLLER}}.js'),
+	eventManager = require('../../events/EventManager'),
+	events = require('../../events/events'),
+	COLORBOX_EVENT = events.COLOR_BOX;
+	
+	var callerObj = {
+		/**
+		 * 초기화
+		 */
+		init: init
+	},
+	self;
+
+	var opts = {
+		templates : {
+			wrap : '{{WRAP}}',
+			template : '{{TEMPLATES}}'
+		},
+		colorbox : '#colorbox',
+		cssClass : {
+			isLoading : 'is-loading',
+			hasAnimate : 'has-animate'
+		}
+	};
+	
+	return callerObj;
+	
+	function init() {
+		Super.init();
+		
+		debug.log(fileName, $, util, controller, eventManager, events, COLORBOX_EVENT);
+
+		self = callerObj;
+		self.opts = opts;
+
+		setElements();
+		setBindEvents();
+	}
+
+	function setElements() {
+		self.templatesWrap = $(self.opts.templates.wrap);
+		self.template = $(self.opts.templates.template);
+
+		self.colorbox = $(self.opts.colorbox);
+		self.selPopBtnInfo = {};
+	}
+
+	function setBindEvents() {
+		$(controller).on({'{{EVENT_TYPE}}'}.WILD_CARD, onControllerListener);
+		eventManager.on(COLORBOX_EVENT.WILD_CARD, onColorBoxAreaListener);
+	}
+
+	// Handlebars 마크업 템플릿 구성
+	function displayData(data) {
+		var source = self.template.html(),
+		template = win.Handlebars.compile(source),
+		insertElements = $(template(data));
+
+		self.templatesWrap.empty()
+							.addClass(self.opts.cssClass.isLoading)
+							.append(insertElements);
+
+		self.templatesWrap.imagesLoaded()
+							.always(function() {
+								self.templatesWrap.removeClass(self.opts.cssClass.isLoading);
+								eventManager.triggerHandler(COLORBOX_EVENT.REFRESH);
+							});
+	}
+
+	function onControllerListener(e, status, response, elements) {
+		var eventType = e.type,
+		dummyData = {},
+		result = response;
+
+		switch(eventType) {
+			default:
+				dummyData = [];
+
+				/*
+				401	Unauthorized
+				403	Forbidden
+				404	Not Found
+				 */
+				switch(status) {
+					case 200:
+						break;
+					default:
+						win.alert('HTTP Status Code ' + status + ' - DummyData 구조 설정');
+						result = dummyData;
+						break;
+				}
+
+				debug.log(fileName, 'onControllerListener', eventType, status, response);
+				displayData(result);
+				break;
+		}
+	}
+
+	function onColorBoxAreaListener(e) {
+		switch(e.type) {
+			case COLORBOX_EVENT.COMPLETE:
+				break;
+			case COLORBOX_EVENT.CLEANUP:
+				break;
+		}
+	}
+};
