462e2e2f3a93c887cffd94de355ac257f1ec329a
hkroh <hkroh@vinyl-x.com>
2016-07-08 14:52:11 +0900

    [ myPage/following ] 컨트롤러 추가/연결

---
+++ src/js/controller/FollowController.js
@@ -0,0 +1,79 @@
+/* jshint node: true, strict: true */
+
+module.exports = ClassFollowController().getInstance();
+
+function ClassFollowController() {
+	'use strict';
+
+	var SuperClass = require('./APIController.js');
+	var Super = SuperClass();
+	
+	var instance;
+	var callerObj;
+
+	var $ = window.jQuery;
+	
+	return {
+		getInstance: function() {
+			if (!instance) instance = ExampleController();
+			return instance;
+		}
+	};
+	
+	function ExampleController() {
+		callerObj = {
+			/**
+			 * 팔로우 리스트
+			 */
+			followsList : followsList,
+			/**
+			 * 팔로우 등록
+			 */
+			addFollows : addFollows,
+			/**
+			 * 팔로우 삭제
+			 */
+			deleteFollows : deleteFollows
+		};		
+		return callerObj;
+	}
+
+
+	function followsList() {
+		Super.callApi('/apis/follows', 'GET', {}, function(status, result) {
+			if (status == 200) {
+				$(callerObj).trigger('followsListResult', [status, result]);
+			} else {
+				Super.handleError('followsList', result);
+				$(callerObj).trigger('followsListResult', [status, result]);
+			}
+		}, false);
+	}
+
+	function addFollows(followTargetCode, followTargetNumber, followTargetSectionCode, elements) {
+		Super.callApi('/apis/follows', 'POST', {
+			'followTargetCode' : followTargetCode,
+			'followTargetNumber' : followTargetNumber,
+			'followTargetSectionCode' : followTargetSectionCode
+		}, function(status, result) {
+			if (status == 200) {
+				$(callerObj).trigger('addFollowsResult', [status, result, elements]);
+			} else {
+				Super.handleError('addFollows', result);
+				$(callerObj).trigger('addFollowsResult', [status, result, elements]);
+			}
+		}, true);
+	}
+
+	function deleteFollows(followNumber, elements) {
+		Super.callApi('/apis/follows/' + followNumber, 'DELETE', {}, function(status, result) {
+			if (status == 200) {
+				$(callerObj).trigger('deleteFollowsResult', [status, result, elements]);
+			} else {
+				Super.handleError('deleteFollows', result);
+				$(callerObj).trigger('deleteFollowsResult', [status, result, elements]);
+			}
+		}, true);
+	}
+}
+
