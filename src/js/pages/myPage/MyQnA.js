39ae20c2796359390f3954288e6b040f6d21227c
Apple-D <eatenapple@gmail.com>
2016-07-10 03:43:29 +0900

    내 의견묻기 내역 API 연결

--- src/js/pages/myPage/MyQnA.js
+++ src/js/pages/myPage/MyQnA.js
@@ -12,6 +12,18 @@ module.exports = function() {
 	var MyPageClass = require('./MyPage.js'),
 	MyPage = MyPageClass();
 	
+	var controller = require('../../controller/OpinionsController.js');
+	$(controller).on('opinionsClassResult', opinionsClassHandler);
+	// $(controller).on('opinionsListResult', myOpinionsHandler);
+	$(controller).on('postAnswerResult', postAnswerResultHandler);
+	$(controller).on('pollAnswerResult', pollAnswerResultHandler);
+
+	var myPageController = require('../../controller/MyPageController.js');
+	$(myPageController).on('myOpinionsResult', myOpinionsHandler);
+
+	var opinionsClassArray;
+	var pollAnswerId;
+
 	var callerObj = {
 		/**
 		 * 초기화
@@ -25,5 +37,107 @@ module.exports = function() {
 		MyPage.init();
 		
 		debug.log(fileName, $, util);
+		
+		controller.opinionsClass();
 	}
+
+	/**
+	 * 의견 주제 목록 핸들링. 이거 받아온 뒤에 의견 리스트 호출해야 함.
+	 */
+	function opinionsClassHandler(e, status, result) {
+		if (status == 200) {
+			var tags = '';
+			opinionsClassArray = new Array();
+			for (var key in result) {
+				var eachTheme = result[key];
+				tags += '<option value="'+eachTheme.opinionClassNumber+'">'+eachTheme.className+'</option>';
+				opinionsClassArray[eachTheme.opinionClassNumber] = eachTheme.className;
+			}
+			$('#opinionThemes').append(tags);
+		}
+		
+		//controller.opinionsList();
+		myPageController.myOpinions();
+	}
+
+	/**
+	 * 내 의견묻기 목록 핸들링
+	 */
+	function myOpinionsHandler(e, status, result) {
+		if (status == 200) {
+			var key = 0;
+			
+			if (result.length > 0) {
+				$.map(result, function(each) {
+					each.opinionClass = opinionsClassArray[each.opinionClassNumber];
+					if (each.answers.length == 0) {
+						each.answerCount = '<p>미답변</p>';
+					} else {
+						each.answerCount = '<p><span class="pointRed">'+each.answers.length+'개</span> 의견</p>';
+					}
+					each.key = key;
+
+					key++;
+				});
+
+				var template = window.Handlebars.compile($('#opinion-template').html());
+				var elements = $(template(result));
+				$('#opinionList').empty().append(elements);
+			}
+			$('.myProfileImage').attr('src', MyPage.Super.Super.loginData.imageUrl);
+
+			$('.writeCommentButton').click(showCommentForm);
+			$('.answerCount').click(pollAnswer);
+			$('.answerForm').submit(answerFormSubmitHandler);
+			
+			$('.except').dotdotdot({watch:'window'});
+		}
+	};
+
+	function showCommentForm(e) {
+		e.preventDefault();
+		if (MyPage.Super.Super.loginData != null) {
+			var pId = $(this).attr('id').substr(18);
+			$('#commentArea'+pId).addClass('showCommentInput');
+		} else {
+			if (confirm('로그인이 필요한 페이지입니다. 로그인하시겠습니까?')) location.href='/member/login.html';
+		}
+	};
+
+	function answerFormSubmitHandler(e) {
+		e.preventDefault();
+		var opinionNumber = $(this).attr('id').substr(10);
+
+		if ($.trim($('#answerBox'+opinionNumber).val()) == '') {
+			alert('내용을 작성해 주세요');
+		} else {
+			controller.postAnswer(opinionNumber, $.trim($('#answerBox'+opinionNumber).val()));
+		}
+	};
+
+	function postAnswerResultHandler(e, status, result) {
+		if (status == 200) {
+			console.log(result);
+			Super.Super.alertPopup('의견묻기', '의견이 등록되었습니다', '확인', function() {
+				location.reload(true);
+			});
+		}
+	};
+
+	function pollAnswer(e){
+		pollAnswerId = $(this).attr('id').substr(6);
+		if (!$(this).hasClass('on')) {
+			controller.pollAnswer(pollAnswerId);
+		}
+	};
+
+	function pollAnswerResultHandler(e, status, result) {
+		if (status == 200) {
+			var newCount = Number($('#answerCount'+pollAnswerId).text())+1;
+			$('#answerCount'+pollAnswerId).text(newCount);
+			$('#answer'+pollAnswerId).addClass('on');
+		} else {
+			alert(status+': '+result.message);
+		}
+	};
 };
