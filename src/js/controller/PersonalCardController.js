54162d53d7a817b68146f1138260bf429aa2fa68
Younbum Lee <eatenapple@gmail.com>
2016-07-11 21:00:33 +0900

    내 정보 API 연결

--- src/js/controller/PersonalCardController.js
+++ src/js/controller/PersonalCardController.js
@@ -19,16 +19,28 @@ function ClassPersonalCardController() {
 	function PersonalCardController() {
 		callerObj = {
 			/**
-			 * 예제
+			 * 카드 리스트
 			 */
-			personalCard: personalCard
+			personalCard: personalCard,
+			/**
+			 * 주소 드롭다운 항목 리스트
+			 */
+			getAddressItems: getAddressItems,
+			/**
+			 * 거주형태 드롭다운 항목 리스트
+			 */
+			getDwellingItem: getDwellingItem,
+			/**
+			 * 개인화 카드 답변
+			 */
+			answer: answer,
 		}
 		
 		return callerObj;	
 	};
 	
 	/**
-	 * 예제
+	 * 카드 리스트
 	 */
 	function personalCard(attr) {
 		Super.callApi('/apis/me/personal', 'GET', {}, function(status, result) {
@@ -38,7 +50,52 @@ function ClassPersonalCardController() {
 				Super.handleError('personalCard', result);
 				$(callerObj).trigger('personalCardResult', [status, result]);
 			}
-		}, false);
+		}, true);
 	};
+
+	/**
+	 * 주소 드롭다운 항목 리스트
+	 */
+	function getAddressItems(target, sido, sigungu) {
+		Super.callApi('/apis/me/personal/region', 'GET', {
+			sido: sido,
+			sigungu: sigungu
+		}, function(status, result) {
+			if (status == 200) {
+				$(callerObj).trigger('addressItemsResult', [200, result.data, target]);
+			} else {
+				Super.handleError('addressItems', result);
+				$(callerObj).trigger('addressItemsResult', [status, result]);
+			}
+		}, true);
+	};
+
+	/**
+	 * 거주형태 드롭다운 항목 리스트
+	 */
+	function getDwellingItem(target) {
+		Super.callApi('/apis/me/personal/dwelling', 'GET', {}, function(status, result) {
+			if (status == 200) {
+				$(callerObj).trigger('dwellingItemsResult', [200, result.data, target]);
+			} else {
+				Super.handleError('dwellingItems', result);
+				$(callerObj).trigger('dwellingItemsResult', [status, result]);
+			}
+		}, true);
+	}
+	
+	/**
+	 * 개인화 카드 답변
+	 */
+	function answer(cardId, answerType, answerObject) {
+		Super.callApi('/apis/me/personal/'+cardId+'?personalType='+answerType, 'POST', answerObject, function(status, result) {
+			if (status == 200) {
+				$(callerObj).trigger('answerResult', [200, result.data]);
+			} else {
+				Super.handleError('answer', result);
+				$(callerObj).trigger('answerResult', [status, result]);
+			}
+		}, true);
+	}
 }
 
--- src/js/pages/myPage/Profile.js
+++ src/js/pages/myPage/Profile.js
@@ -14,11 +14,19 @@ module.exports = function() {
 
 	var controller = require('../../controller/PersonalCardController');
 	$(controller).on('personalCardResult', personalCardResultHandler);
+	$(controller).on('addressItemsResult', addressItemsResultHandler);
+	$(controller).on('dwellingItemsResult', dwellingItemsResultHandler);
+	$(controller).on('answerResult', answerResultHandler); 
 	
-	var dropDownMenu =  require('../../components/DropDownMenu.js');
 
 	var ApiController = require('../../controller/APIController');
 	var apiController = ApiController();
+
+	var DropDownMenu = require('../../components/DropDownMenu.js');
+	var CardList = require('../../components/CardList.js');
+	var cardList = CardList();
+
+	var selectedItems = new Array();
 	
 	var callerObj = {
 		/**
@@ -31,58 +39,195 @@ module.exports = function() {
 	
 	function init() {
 		MyPage.init();
-		
-		debug.log(fileName, $, util);
+
+		cardList.init('#cardWrap');	// 카드 리스트
 
 		controller.personalCard();
 	}
 
 	function personalCardResultHandler(e, status, result) {
 		$.map(result, function(each) {
-			console.log(each);
+			//console.log(each);
 
 			if (each.questionContents == undefined) each.questionContents = each.cardName;
 			switch(each.registerTypeCode) {
 				case "DP_CARD_REGISTER_TYPE_03":
-					//
+					switch(each.apiUrl.split('/').pop()) {
+						case 'region':
+							each.cardClass = 'addressCard';
+							if (each.memberAnswerYn == 'Y') selectedItems[each.cardNumber] = each.myPersonalRegionMemberAnswer;
+							break;
+						case 'dwelling':
+							each.cardClass = 'dwellingCard';
+							if (each.memberAnswerYn == 'Y') selectedItems[each.cardNumber] = each.myPersonalDwellingMemberAnswer;
+							break;
+					}
+					
 					break;
 				case "DP_CARD_REGISTER_TYPE_04":
-					//
+					each.cardClass = 'selectCard';
 					break;
 			}
 		});
 
 		var template = window.Handlebars.compile($('#personal-info-card-template').html());
-		var elements = $(template(result));
-		$('#myInfo').empty().append(elements);
+		cardList.html(template(result));
 
-		$('#myInfo .card.cardType09').each(function(idx){
-			var cardObj = $(this);
-			if (cardObj.data().apiUrl != '') apiController.callApi(cardObj.data().apiUrl, 'GET', {}, function(status, result) {
-				console.log(cardObj.attr('id'), result.data);
-			}, true);
+		DropDownMenu.init();
+		initAddressCard();
+		initDwellingCard();
+
+		$('.cardCollect > li').click(function(e){
+			e.preventDefault();
+			console.log( $(this).data().answerContent );
+			e.stopPropagation();
+		});
+	};
+
+	function initDwellingCard() {
+		$('.dwellingCard').each(function(idx){
+			var selectedItem = selectedItems[$(this).attr('id').substr(12)];
+			if (selectedItem != undefined) {
+				$(this).find('.dwellingFormDrop').html('<ul class="drop" data-prevent="true"><li><a href="#">'+selectedItem.dwellingFormCodeName+'</a></li></ul>');
+				$(this).find('.dwellingPyeongDrop').html('<ul class="drop" data-prevent="true"><li><a href="#">'+selectedItem.dwellingPyeongCodeName+'</a></li></ul>');
+			}
+			controller.getDwellingItem($(this));
 		});
 
-		dropDownMenu.init();
-	}
+		$('.dwellingCard .alignBox').on(DropDownMenu.EVENT.CHANGE, function(e, data) {
+			var cardTarget = $(e.target.parentNode.parentNode.parentNode.parentNode.parentNode);
+			var cardId = cardTarget.attr('id').substr(12);
+			var dataKey = String($(this).data().dataKey);
+			
+			if (selectedItems[cardId] == undefined) selectedItems[cardId] = {};
+			switch(dataKey) {
+				case 'dwellingForm': selectedItems[cardId].dwellingFormCode = data.values[0]; break;
+				case 'dwellingPyeong': selectedItems[cardId].dwellingPyeongCode = data.values[0]; break;
+			}
 
-	
+			if (selectedItems[cardId].dwellingFormCode != undefined && selectedItems[cardId].dwellingPyeongCode != undefined) {
+				controller.answer(cardId, 'BM_PERSONAL_TYPE_02', {
+					"personalAnswer": {
+						"answerSequences": [0],
+						"questionNumber": cardId,
+					},
+					"personalDwelling": selectedItems[cardId]
+				});
+			}
+		});
+	};
 
-	/* {
-		"personalAnswer": {
-			"answerSequences": [
-				0
-			],
-			"questionNumber": 0
-		},
-		"personalDwelling": {
-			"dwellingFormCode": "string",
-			"dwellingPyeongCode": "string"
-		},
-		"personalRegion": {
-			"dong": "string",
-			"sido": "string",
-			"sigungu": "string"
+	function dwellingItemsResultHandler(e, status, result, target) {
+		for (var key in result) {
+			var targetBox = target.find('.'+key+'Drop');
+			var selectedItem = selectedItems[target.attr('id').substr(12)];
+			
+			var label = '선택해주세요';
+			if (selectedItem != undefined) label = selectedItem[key+'CodeName'];
+			
+			var eachDrop = result[key];
+			var tags = '<ul class="drop" data-prevent="true"><li><a href="#">'+label+'</a></li>';
+			for (var valueKey in eachDrop) {
+				var eachItem = eachDrop[valueKey];
+				tags += '<li><a href="#" data-value="'+eachItem.code+'">'+eachItem.codeDesc+'</a></li>';
+			}
+			tags += '</ul>';
+
+			targetBox.html(tags);
+			targetBox.data().dataKey = key;
 		}
-	} */
+		
+		DropDownMenu.refresh();
+	};
+
+	function initAddressCard() {
+		$('.addressCard').each(function(idx){
+			var selectedItem = selectedItems[$(this).attr('id').substr(12)];
+			if (selectedItem != undefined) {
+				$(this).find('.addressDrop1').html('<ul class="drop" data-prevent="true"><li><a href="#">'+selectedItem.sido+'</a></li></ul>');
+				$(this).find('.addressDrop2').html('<ul class="drop" data-prevent="true"><li><a href="#">'+selectedItem.sigungu+'</a></li></ul>');
+				$(this).find('.addressDrop3').html('<ul class="drop" data-prevent="true"><li><a href="#">'+selectedItem.dong+'</a></li></ul>');
+			}
+			controller.getAddressItems($(this));
+		});
+
+		$('.addressDrop1').on(DropDownMenu.EVENT.CHANGE, function(e, data) {
+			var cardTarget = $(e.target.parentNode.parentNode.parentNode.parentNode.parentNode);
+			var cardId = cardTarget.attr('id').substr(12);
+			selectedItems[cardId] = {};
+			selectedItems[cardId].sido = data.values[0];
+
+			cardTarget.find('.addressDrop3').html('<ul class="drop" data-prevent="true"><li><a href="#">읍면동</a></li></ul>');
+			controller.getAddressItems(cardTarget, selectedItems[cardId].sido);
+		});
+
+		$('.addressDrop2').on(DropDownMenu.EVENT.CHANGE, function(e, data) {
+			var cardTarget = $(e.target.parentNode.parentNode.parentNode.parentNode.parentNode);
+			var cardId = cardTarget.attr('id').substr(12);
+			selectedItems[cardId].sigungu = data.values[0];
+
+			controller.getAddressItems(cardTarget, selectedItems[cardId].sido, selectedItems[cardId].sigungu);
+		});
+
+		$('.addressDrop3').on(DropDownMenu.EVENT.CHANGE, function(e, data) {
+			var cardTarget = $(e.target.parentNode.parentNode.parentNode.parentNode.parentNode);
+			var cardId = cardTarget.attr('id').substr(12);
+			selectedItems[cardId].dong = data.values[0];
+
+			// BM_PERSONAL_TYPE_02: 주거, BM_PERSONAL_TYPE_03: 나머지 응답
+			controller.answer(cardId, 'BM_PERSONAL_TYPE_01', {
+				"personalAnswer": {
+					"answerSequences": [0],
+					"questionNumber": cardId,
+				},
+				"personalRegion": selectedItems[cardId]
+			});
+		});
+	};
+
+	function addressItemsResultHandler(e, status, result, target) {
+		var targetBox, label, key, each;
+		var tags = '<ul class="drop" data-prevent="true">';
+
+		if (result.regionList[0].sido != result.regionList[1].sido) {
+			var selectedItem = selectedItems[target.attr('id').substr(12)];
+			label = (selectedItem == undefined ? '시/도' : selectedItem.sido);
+			targetBox = target.find('.addressDrop1');
+
+			tags += '<li><a href="#">'+label+'</a></li>';
+			for (key in result.regionList) {
+				each = result.regionList[key];
+				tags += '<li><a href="#" data-value="'+each.sido+'">'+each.sido+'</a></li>';
+			}
+		} else if (result.regionList[0].sigungu != result.regionList[1].sigungu) {
+			label = '시군구';
+			targetBox = target.find('.addressDrop2');
+
+			tags += '<li><a href="#">'+label+'</a></li>';
+			for (key in result.regionList) {
+				each = result.regionList[key];
+				tags += '<li><a href="#" data-value="'+each.sigungu+'">'+each.sigungu+'</a></li>';
+			}
+		} else {
+			label = '읍면동';
+			targetBox = target.find('.addressDrop3');
+
+			tags += '<li><a href="#">'+label+'</a></li>';
+			for (key in result.regionList) {
+				each = result.regionList[key];
+				tags += '<li><a href="#" data-value="'+each.dong+'">'+each.dong+'</a></li>';
+			}
+		}
+		tags += '</ul>';
+
+		targetBox.html(tags);
+		DropDownMenu.refresh();
+	};
+
+	function answerResultHandler(e, status, result) {
+		if (status == 200) {
+			alert('답변이 등록되었습니다');
+			location.reload(true);
+		}
+	};
 };
