e1c89aca3600c249a7c7d6cdafe723c0ea985d26
hkroh <hkroh@vinyl-x.com>
2016-07-12 22:46:07 +0900

    [ myPage ] 배송주문 API 연결

--- src/js/pages/myPage/Order.js
+++ src/js/pages/myPage/Order.js
@@ -13,12 +13,12 @@ module.exports = function() {
 	Super = SuperClass(),
 	DatePickerClass = require('../../components/DatePicker.js'),
 	DatePicker = DatePickerClass(),
-	dropDownMenu =  require('../../components/DropDownMenu.js'),
 	controller = require('../../controller/OrderController.js'),
 	eventManager = require('../../events/EventManager'),
 	events = require('../../events/events'),
 	COLORBOX_EVENT = events.COLOR_BOX,
-	ORDER_EVENT = events.ORDER;
+	ORDER_EVENT = events.ORDER,
+	DROPDOWNMENU_EVENT = events.DROPDOWN_MENU;
 	
 	var callerObj = {
 		/**
@@ -30,14 +30,25 @@ module.exports = function() {
 
 	var opts = {
 		templates : {
-			wrap : '.test',
-			template : '.test'
+			wrap : '.js-order-wrap',
+			template : '#order-list-templates'
 		},
 		colorbox : '#colorbox',
 		cssClass : {
 			isLoading : 'is-loading',
 			hasAnimate : 'has-animate'
-		}
+		},
+		rangePicker : {
+			wrap : '.js-range-picker',
+			altFrom : '.js-alt-from',
+			altTo : '.js-alt-to'
+		},
+		search : {
+			wrap : '.js-order-search',
+			inp : '.js-inp',
+			submit : '.js-submit',
+		},
+		dateFormat : 'YYYYMMDD'
 	};
 	
 	return callerObj;
@@ -50,11 +61,26 @@ module.exports = function() {
 		self = callerObj;
 		self.opts = opts;
 
-		controller.myOrdersList();
+		self.isDebug = true;
+		self.isFilter = false;
 
 		setElements();
 		setBindEvents();
 		setRangePicker();
+
+		getOrderList();
+	}
+
+	function getOrderList(keyword, deliveryStateCode) {
+		keyword = keyword || '';
+		deliveryStateCode = deliveryStateCode || '';
+
+		controller.myOrdersList(
+			win.moment(self.rangeAltFrom.val()).format(self.opts.dateFormat),
+			win.moment(self.rangeAltTo.val()).format(self.opts.dateFormat),
+			keyword,
+			deliveryStateCode
+		);
 	}
 
 	function setElements() {
@@ -63,14 +89,52 @@ module.exports = function() {
 
 		self.colorbox = $(self.opts.colorbox);
 		self.selPopBtnInfo = {};
+
+		self.rangePicker = $(self.opts.rangePicker.wrap);
+		self.rangeAltFrom = self.rangePicker.find(self.opts.rangePicker.altFrom);
+		self.rangeAltTo = self.rangePicker.find(self.opts.rangePicker.altTo);
+
+		self.search = $(self.opts.search.wrap);
+		self.searchInp = self.search.find(self.opts.search.inp);
+		self.searchSubmit = self.search.find(self.opts.search.submit);
+
+		self.selPopBtnInfo = {};
 	}
 
 	function setBindEvents() {
 		$(controller).on(ORDER_EVENT.WILD_CARD, onControllerListener);
 		eventManager.on(COLORBOX_EVENT.WILD_CARD, onColorBoxAreaListener);
 
-		$('.drop').filter(':not(.dropChk)').on(dropDownMenu.EVENT.CHANGE, onDropMenuChange);
-		$('.dropChk').on(dropDownMenu.EVENT.CHANGE, onDropCheckMenuChange);
+		$('.dropChk').on(DROPDOWNMENU_EVENT.CHANGE, onDropCheckMenuChange);
+
+		self.search.on('submit', function(e) {
+			e.preventDefault();
+		});
+		self.searchSubmit.on('click', onSearch);
+		self.searchInp.on('keydown', onSearch);
+
+		self.templatesWrap.on('click', '.js-btn', onWrapPopBtnClick);
+	}
+
+	function onWrapPopBtnClick(e) {
+		e.preventDefault();
+
+		var target = $(e.currentTarget),
+		isConfirm = false,
+		info = target.closest('[data-order-info]').data('order-info');
+
+		self.selPopBtnInfo = {
+			target : target,
+			info : info
+		};
+
+		if (target.hasClass('js-order-confirm')) {
+			isConfirm = win.confirm('선택하신 상품의 구매를 확정하시겠습니까?');
+		}
+
+		if (isConfirm) {
+			controller.orderConfirm(self.selPopBtnInfo.info.orderNumber, self.selPopBtnInfo.info.orderProductSequence);
+		}
 	}
 
 	// Handlebars 마크업 템플릿 구성
@@ -90,14 +154,130 @@ module.exports = function() {
 							});
 	}
 
-	function onControllerListener(e, status, response, elements) {
+	function setColoboxEvevnts() {
+		// 취소신청
+		if (self.colorbox.hasClass('popOrderCancelRequest')) {
+			controller.orderDetail(self.selPopBtnInfo.info.orderNumber);
+		}
+
+		// 배송추적
+		if (self.colorbox.hasClass('popOrderDelivery')) {
+			controller.orderTrackingInfo(
+				self.selPopBtnInfo.info.orderNumber,
+				self.selPopBtnInfo.info.orderProductSequence,
+				self.selPopBtnInfo.info.deliveryNumber,
+				self.selPopBtnInfo.info.orderNumber
+			);
+		}
+
+		self.colorbox.find('.btnColor02').on('click', function(e) {
+			e.preventDefault();
+			testResult();
+		});
+	}
+
+	function testResult() {
+		win.alert('임시처리 결과처리 - location.reload');
+		win.location.reload();
+	}
+
+	function destroyColoboxEvevnts() {
+	}
+
+	function onColorBoxAreaListener(e) {
+		switch(e.type) {
+			case COLORBOX_EVENT.COMPLETE:
+				setColoboxEvevnts();
+				break;
+			case COLORBOX_EVENT.CLEANUP:
+				destroyColoboxEvevnts();
+				break;
+		}
+	}
+
+	function onDropCheckMenuChange(e, data) {
+		var target = $(e.target);
+
+		debug.log(fileName, 'onDropCheckMenuChange', target, target.val(), data);
+		getOrderList(self.searchInp.val(), data.values.join(','));
+	}
+
+	function onSearch(e) {
+		if (e.type === 'keydown' && e.which !== $.ui.keyCode.ENTER) {
+			return;
+		}
+
+		e.preventDefault();
+
+		if (!self.searchInp.val() || self.searchInp.val() === ' ') {
+			win.alert('검색어를 입력하세요.');
+			self.searchInp.val('').focus();
+			return;
+		}
+		getOrderList(self.searchInp.val());
+	}
+
+	function setRangePicker() {
+		DatePicker.init({
+			type : 'range',
+			range : {
+				from : {
+					wrap : self.rangePicker,
+					picker : self.rangePicker.find('.js-picker-from'),
+					altField : self.rangePicker.find('.js-alt-from'),
+					button : self.rangePicker.find('.js-btn-from'),
+					minDate : null
+				},
+				to : {
+					wrap : self.rangePicker,
+					picker : self.rangePicker.find('.js-picker-to'),
+					altField : self.rangePicker.find('.js-alt-to'),
+					button : self.rangePicker.find('.js-btn-to'),
+					maxDate : 0
+				}
+			}
+		});
+
+		$('.sortTerm li a').click(function(e) {
+			e.preventDefault();
+			$(this).addClass('on').parent().siblings('li').find('a').removeClass('on');
+			switch($(this).text()) {
+				case '1주일':
+					$('.js-picker-from').datepicker('setDate', win.moment().subtract(7, 'days').format('YYYY-MM-DD'));
+					break;
+				case '2주일':
+					$('.js-picker-from').datepicker('setDate', win.moment().subtract(14, 'days').format('YYYY-MM-DD'));
+					break;
+				case '1개월':
+					$('.js-picker-from').datepicker('setDate', win.moment().subtract(1, 'months').format('YYYY-MM-DD'));
+					break;
+				case '3개월':
+					$('.js-picker-from').datepicker('setDate', win.moment().subtract(3, 'months').format('YYYY-MM-DD'));
+					break;
+				case '6개월':
+					$('.js-picker-from').datepicker('setDate', win.moment().subtract(6, 'months').format('YYYY-MM-DD'));
+					break;
+			}
+			$('.js-picker-to').datepicker('setDate', win.moment().format('YYYY-MM-DD'));
+			$('.js-picker-to').datepicker('option', 'minDate', win.moment($('.js-alt-from').val()).format('YYYY-MM-DD'));
+			e.stopPropagation();
+
+			getOrderList();
+		});
+	}
+
+	function onControllerListener(e, status, response/*, elements*/) {
 		var eventType = e.type,
 		dummyData = {},
 		result = response;
 
 		switch(eventType) {
-			default:
-				dummyData = [];
+			case ORDER_EVENT.ORDER_LIST:
+				dummyData = {"cellPhoneNum":"string","discountPrice":0,"generalPhoneNum":"string","listOrderItems":[{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_01","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"},{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_02","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"},{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_03","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"},{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_04","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"},{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_05","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"},{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_06","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"}],"optionNumber":"string","orderCancelCode":"string","orderCencelDesc":"string","orderDate":"string","orderNumber":0,"ordererEmail":"string","ordererId":"string","ordererName":"string","productNumber":"string","receiverName":"string","sellerId":"string","sellerName":"string","totalPaymentPrice":0,"totalPrice":0};
+				if (self.isDebug && self.isFilter) {
+					dummyData = {"cellPhoneNum":"string","discountPrice":0,"generalPhoneNum":"string","listOrderItems":[{"deliveryAddressNumber":"string","deliveryFee":"string","deliveryNumber":"string","deliveryState":"string","deliveryStateCode":"SL_ORDER_STATE_01","discountPrice":0,"itemCount":0,"itemPrice":0,"optionName":"string","optionNumber":"string","orderItemReceiver":{"addrSeq":"string","addressManagementName":"string","deliveryAddressNumber":"string","deliveryRequestMemo":"string","receiverDetailAddress":"string","receiverLotBaseAddress":"string","receiverName":"string","receiverRoadBaseAddress":"string","receiverTel1":"string","receiverTel2":"string","zipCode":"string"},"orderProductSequence":"string","paymentPrice":0,"productImageUrl":"../images/temp09.jpg","productName":"string","productNumber":"string","receiverName":"string","sellerName":"string","sellerNumber":"string"}],"optionNumber":"string","orderCancelCode":"string","orderCencelDesc":"string","orderDate":"string","orderNumber":0,"ordererEmail":"string","ordererId":"string","ordererName":"string","productNumber":"string","receiverName":"string","sellerId":"string","sellerName":"string","totalPaymentPrice":0,"totalPrice":0};
+				}
+				self.isFilter = !self.isFilter;
 
 				/*
 				401	Unauthorized
@@ -108,58 +288,55 @@ module.exports = function() {
 					case 200:
 						break;
 					default:
-						win.alert('HTTP Status Code ' + status + ' - DummyData 구조 설정');
-						result = dummyData;
+						win.alert('HTTP Status Code ' + status);
 						break;
+				}
+
+				result.totalPaymentPriceDesc = util.currencyFormat(parseInt(result.totalPaymentPrice, 10));
+
+				if (result.listOrderItems) {
+					$.each(result.listOrderItems, function(index, val) {
+						val.itemPriceDesc = util.currencyFormat(parseInt(val.itemPrice, 10));
+					});
 				}
 
 				debug.log(fileName, 'onControllerListener', eventType, status, response);
 				displayData(result);
 				break;
-		}
-	}
 
-	function onColorBoxAreaListener(e) {
-		switch(e.type) {
-			case COLORBOX_EVENT.COMPLETE:
-				break;
-			case COLORBOX_EVENT.CLEANUP:
-				break;
-		}
-	}
-
-	function onDropMenuChange(e, data) {
-		var target = $(e.target);
-
-		debug.log(fileName, 'onDropMenuChange', target, target.val(), data);
-	}
-
-	function onDropCheckMenuChange(e, data) {
-		var target = $(e.target);
-
-		debug.log(fileName, 'onDropCheckMenuChange', target, target.val(), data);
-	}
-
-	function setRangePicker() {
-		var rangePicker = $('.js-range-picker');
-		DatePicker.init({
-			type : 'range',
-			range : {
-				from : {
-					wrap : rangePicker,
-					picker : rangePicker.find('.js-picker-from'),
-					altField : rangePicker.find('.js-alt-from'),
-					button : rangePicker.find('.js-btn-from'),
-					minDate : null
-				},
-				to : {
-					wrap : rangePicker,
-					picker : rangePicker.find('.js-picker-to'),
-					altField : rangePicker.find('.js-alt-to'),
-					button : rangePicker.find('.js-btn-to'),
-					maxDate : 0
+			case ORDER_EVENT.ORDER_CONFIRM:
+				switch(status) {
+					case 200:
+						window.alert('구매가 확정되었습니다.');
+						break;
+					default:
+						win.alert('HTTP Status Code ' + status);
+						break;
 				}
-			}
-		});
+				debug.log(fileName, 'onControllerListener', eventType, status, response);
+				break;
+
+			case ORDER_EVENT.ORDER_DETAIL:
+				switch(status) {
+					case 200:
+						break;
+					default:
+						win.alert('HTTP Status Code ' + status);
+						break;
+				}
+				debug.log(fileName, 'onControllerListener', eventType, status, response);
+				break;
+
+			case ORDER_EVENT.ORDER_TRACKING:
+				switch(status) {
+					case 200:
+						break;
+					default:
+						win.alert('HTTP Status Code ' + status);
+						break;
+				}
+				debug.log(fileName, 'onControllerListener', eventType, status, response);
+				break;
+		}
 	}
 };
--- src/js/pages/myPage/OrderDetail.js
+++ src/js/pages/myPage/OrderDetail.js
@@ -9,21 +9,339 @@ module.exports = function() {
 	util = require('../../utils/Util.js'),
 	fileName = 'myPage/OrderDetail.js';
 
-	var MyPageClass = require('./MyPage.js'),
-	MyPage = MyPageClass();
+	var SuperClass = require('../Page.js'),
+	Super = SuperClass(),
+	controller = require('../../controller/OrderController.js'),
+	eventManager = require('../../events/EventManager'),
+	events = require('../../events/events'),
+	COLORBOX_EVENT = events.COLOR_BOX,
+	ORDER_EVENT = events.ORDER,
+	DROPDOWNMENU_EVENT = events.DROPDOWN_MENU,
+	HTMLPOPUP_EVENT = events.HTML_POPUP;
 	
 	var callerObj = {
 		/**
 		 * 초기화
 		 */
 		init: init
+	},
+	self;
+
+	var opts = {
+		templates : {
+			wrap : '.js-detail-wrap',
+			template : '#order-detail-templates'
+		},
+		colorbox : '#colorbox',
+		cssClass : {
+			isLoading : 'is-loading',
+			hasAnimate : 'has-animate'
+		}
 	};
 	
 	return callerObj;
 	
 	function init() {
-		MyPage.init();
+		Super.init();
 		
-		debug.log(fileName, $, util);
+		debug.log(fileName, $, util, controller, eventManager, events, COLORBOX_EVENT);
+
+		self = callerObj;
+		self.opts = opts;
+
+		setElements();
+		setBindEvents();
+
+		self.orderNumber = util.getUrlVar().orderNumber;
+
+		if (!self.orderNumber) {
+			win.alert('유효하지 않은 접근입니다.');
+			location.href = '/';
+			return;
+		}
+
+		controller.orderDetail(util.getUrlVar().orderNumber);
+	}
+
+	function setElements() {
+		self.templatesWrap = $(self.opts.templates.wrap);
+		self.template = $(self.opts.templates.template);
+
+		self.colorbox = $(self.opts.colorbox);
+		self.selPopBtnInfo = {};
+
+		self.selPopBtnInfo = {};
+	}
+
+	function setBindEvents() {
+		$(controller).on(ORDER_EVENT.WILD_CARD, onControllerListener);
+		eventManager.on(COLORBOX_EVENT.WILD_CARD, onColorBoxAreaListener);
+
+		$('.dropChk').on(DROPDOWNMENU_EVENT.CHANGE, onDropCheckMenuChange);
+
+		self.templatesWrap.on('click', '.js-btn', onWrapPopBtnClick);
+	}
+
+	function onWrapPopBtnClick(e) {
+		e.preventDefault();
+
+		var target = $(e.currentTarget),
+		info = target.data('order-info');
+
+		self.selPopBtnInfo = {
+			target : target,
+			info : info
+		};
+
+		if (target.hasClass('js-order-cancel')) {
+			var list = $('[data-chk-group=\'orderDetail\']').not('[data-chk-role=\'chkAll\']').filter('.on'),
+			deleteList = [];
+
+			$.each(list, function() {
+				deleteList.push($(this).data('order-number'));
+			});
+
+			if (!deleteList.length) {
+				return;
+			}
+
+			// 주문 취소 신청 처리
+			// controller.orderCancel(deleteList, '', '');
+
+			eventManager.trigger(HTMLPOPUP_EVENT.OPEN, [
+				'../../_popup/popOrderCancelRequest.html',
+				895,
+				'popEdge',
+				{
+					onOpen: function() {
+						controller.orderDetail(deleteList, 'COLOR_BOX');
+					}
+				}
+			]);
+
+		}
+	}
+
+	// Handlebars 마크업 템플릿 구성
+	function displayData(data, type) {
+		var _template = self.template,
+		_templatesWrap = self.templatesWrap;
+
+		if (type === 'COLOR_BOX') {
+			_template = self.colorbox.find('#cancel-request-templates');
+			_templatesWrap =  self.colorbox.find('.js-cancelRequest-wrap');
+		}
+
+		var source = _template.html(),
+		template = win.Handlebars.compile(source),
+		insertElements = $(template(data));
+
+		_templatesWrap.empty()
+							.addClass(self.opts.cssClass.isLoading)
+							.append(insertElements);
+
+		_templatesWrap.imagesLoaded()
+							.always(function() {
+								_templatesWrap.removeClass(self.opts.cssClass.isLoading);
+								eventManager.triggerHandler(COLORBOX_EVENT.REFRESH);
+								eventManager.triggerHandler(COLORBOX_EVENT.RESIZE);
+							});
+
+		if (type === 'COLOR_BOX') {
+			self.colorbox.find('form').on('submit', function(e) {e.preventDefault();});
+			self.colorbox.find('.js-cancel-submit').on('click', function(e) {
+				e.preventDefault();
+
+				var forms = self.colorbox.find('.js-cancel-form'),
+				isValid = false,
+				cancelType, cancelReson;
+
+				$.each(forms, function() {
+					cancelType = $(this).find('.js-type').val();
+					cancelReson = $(this).find('.js-inp').val();
+					if (cancelType && cancelReson) {
+						isValid = true;
+					} else {
+						isValid = false;
+						return false;
+					}
+				});
+
+				if (!isValid) {
+					win.alert('취소 사유를 선택/입력 해주세요.');
+					return;
+				}
+
+				$.each(forms, function() {
+					cancelType = $(this).find('.js-type').val();
+					cancelReson = $(this).find('.js-inp').val();
+
+					controller.orderCancel(
+						self.selPopBtnInfo.info.orderNumber,
+						{
+							"accountAuthDatetime": "2016-04-01",
+							"accountAuthYn": "Y",
+							"addDeliveryChargeTotal": 0,
+							"claimDeliveryChargeTotal": 0,
+							"claimReasonCode": cancelType,
+							"claimReasonStatement": "string",
+							"claimTypeCode": "string",
+							"orderNumber": 0,
+							"refundAccountNumber": 0,
+							"refundBankCode": "string",
+							"refundDepositorName": cancelReson
+						},
+						[
+							{
+								"addDeliveryCharge": 0,
+								"claimDeliveryCharge": 0,
+								"claimNumber": 0,
+								"claimProcessDatetime": "string",
+								"claimProcessQuantity": 0,
+								"claimProductAmount": 0,
+								"claimRequestQuantity": 0,
+								"claimStateCode": "string",
+								"claimStateReason": "string",
+								"deliveryChargePaymentCode": "string",
+								"orderNumber": 0,
+								"orderProductSequence": "string"
+							}
+						]
+					);
+				});
+			});
+		}
+	}
+
+	function setColoboxEvevnts() {
+		if (self.colorbox.hasClass('popOrderCancelRequest')) {
+		}
+	}
+
+	function testResult() {
+		win.alert('임시처리 결과처리 - location.reload');
+		win.location.reload();
+	}
+
+	function destroyColoboxEvevnts() {
+	}
+
+	function onColorBoxAreaListener(e) {
+		switch(e.type) {
+			case COLORBOX_EVENT.COMPLETE:
+				setColoboxEvevnts();
+				break;
+			case COLORBOX_EVENT.CLEANUP:
+				destroyColoboxEvevnts();
+				break;
+		}
+	}
+
+	function onDropCheckMenuChange(e, data) {
+		var target = $(e.target);
+
+		debug.log(fileName, 'onDropCheckMenuChange', target, target.val(), data);
+	}
+
+	function onControllerListener(e, status, response, type) {
+		var eventType = e.type,
+		dummyData = {},
+		result = response;
+
+		switch(eventType) {
+			case ORDER_EVENT.ORDER_DETAIL:
+				dummyData = {
+					"cellPhoneNum": "string",
+					"discountPrice": 0,
+					"generalPhoneNum": "string",
+					"listOrderItems": [
+						{
+							"deliveryAddressNumber": "string",
+							"deliveryFee": "string",
+							"deliveryNumber": "string",
+							"deliveryState": "string",
+							"deliveryStateCode": "string",
+							"discountPrice": 0,
+							"itemCount": 0,
+							"itemPrice": 0,
+							"optionName": "string",
+							"optionNumber": "string",
+							"orderItemReceiver": {
+								"addrSeq": "string",
+								"addressManagementName": "string",
+								"deliveryAddressNumber": "string",
+								"deliveryRequestMemo": "string",
+								"receiverDetailAddress": "string",
+								"receiverLotBaseAddress": "string",
+								"receiverName": "string",
+								"receiverRoadBaseAddress": "string",
+								"receiverTel1": "string",
+								"receiverTel2": "string",
+								"zipCode": "string"
+							},
+							"orderProductSequence": "string",
+							"paymentPrice": 0,
+							"productImageUrl": "string",
+							"productName": "string",
+							"productNumber": "string",
+							"receiverName": "string",
+							"sellerName": "string",
+							"sellerNumber": "string"
+						}
+					],
+					"optionNumber": "string",
+					"orderCancelCode": "string",
+					"orderCencelDesc": "string",
+					"orderDate": "string",
+					"orderNumber": 0,
+					"ordererEmail": "string",
+					"ordererId": "string",
+					"ordererName": "string",
+					"productNumber": "string",
+					"receiverName": "string",
+					"sellerId": "string",
+					"sellerName": "string",
+					"totalPaymentPrice": 0,
+					"totalPrice": 0
+				};
+
+				switch(status) {
+					case 200:
+						break;
+					default:
+						win.alert('HTTP Status Code ' + status);
+						break;
+				}
+
+				result = dummyData;
+
+				result.totalPaymentPriceDesc = util.currencyFormat(parseInt(result.totalPaymentPrice, 10));
+				result.discountPriceDesc = util.currencyFormat(parseInt(result.discountPriceDesc, 10));
+
+				if (result.listOrderItems) {
+					$.each(result.listOrderItems, function(index, listOrderItems) {
+						listOrderItems.itemPriceDesc = util.currencyFormat(parseInt(listOrderItems.itemPrice, 10));
+						listOrderItems.discountPriceDesc = util.currencyFormat(parseInt(listOrderItems.discountPrice, 10));
+						listOrderItems.deliveryFeeDesc = util.currencyFormat(parseInt(listOrderItems.deliveryFee, 10));
+					});
+				}
+
+				debug.log(fileName, 'onControllerListener', eventType, status, response, result);
+				displayData(result, type);
+				break;
+
+			case ORDER_EVENT.ORDER_CANCEL:
+				switch(status) {
+					case 200:
+						win.alert('취소 신청이 완료되었습니다.');
+						location.reload();
+						break;
+					default:
+						win.alert('HTTP Status Code ' + status);
+						break;
+				}
+				debug.log(fileName, 'onControllerListener', eventType, status, response, result);
+				break;
+		}
 	}
 };
