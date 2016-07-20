18b349bfc9cbc229e1850790fcc1ffa5b2030f26
hkroh <hkroh@vinyl-x.com>
2016-07-20 20:12:40 +0900

    [ mobile - myPage ]  myCartShop / myCartNewForm - js 적용

--- src/js/pagesMobile/myPage/MyCartNewForm.js
+++ src/js/pagesMobile/myPage/MyCartNewForm.js
@@ -7,7 +7,7 @@ module.exports = function() {
 	$ = win.jQuery,
 	debug = require('../../utils/Console.js'),
 	util = require('../../utils/Util.js'),
-	fileName = 'myPage/MyCartNewForm.js';
+	fileName = 'myPage/MyCartShop.js';
 
 	var MyPageClass = require('./MyPage.js'),
 	MyPage = MyPageClass(),
@@ -15,9 +15,15 @@ module.exports = function() {
 	eventManager = require('../../events/EventManager'),
 	events = require('../../events/events'),
 	COLORBOX_EVENT = events.COLOR_BOX,
+	OPTIONNUM_EVENT = events.OPTION_NUM,
 	CHECKBOX_EVENT = events.CHECK_BOX,
 	CART_EVENT = events.CART;
+	
+	var loginController = require('../../controller/LoginController');
+	var loginDataModel = require('../../model/LoginModel');
 
+	var orderData;
+	
 	var callerObj = {
 		/**
 		 * 초기화
@@ -27,16 +33,26 @@ module.exports = function() {
 	self;
 
 	var opts = {
-		newFormList : {
+		cartList : {
 			wrap : '.js-myCartNewForm-list',
 			totalPrice : '.js-list-totalPrice'
 		},
+		cartOrderInfo : {
+			wrap : '.js-myCartNewForm-orderInfo',
+			price : '.js-orderInfo-price',
+			discount : '.js-dicount-price',
+			delivery : '.js-order-delivery',
+			totalPrice : '.js-orderInfo-totalPrice',
+			moreBtn : '.js-orderInfo-more',
+			moreWrap : '.js-orderInfo-more-wrap'
+		},
 		templates : {
-			wrap : '.singleCard',
+			wrap : '.js-myCartNewForm-list-wrap',
 			template : '#myCartNewForm-list-templates'
 		},
 		listSize : '.js-list-size',
 		listDel : '.js-list-delete',
+		optionNum : '.optionNum .num',
 		colorbox : '#colorbox',
 		cssClass : {
 			isLoading : 'is-loading',
@@ -58,7 +74,7 @@ module.exports = function() {
 		setBindEvents();
 
 		// myCartNewForm 리스트 조회
-		var productSectionCode = 'PD_PROD_TYPE_02';
+		var productSectionCode = 'PD_PROD_SVC_SECTION_02';
 		controller.myCartList(productSectionCode);
 	}
 
@@ -67,20 +83,33 @@ module.exports = function() {
 		self.template = $(self.opts.templates.template);
 
 		self.colorbox = $(self.opts.colorbox);
+		self.chkedList = null;
 	}
 
 	function setBindEvents() {
 		$(controller).on(CART_EVENT.WILD_CARD, onControllerListener);
 
+		eventManager.on(OPTIONNUM_EVENT.CHANGE, onOptionNumChange);
 		eventManager.on(CHECKBOX_EVENT.CHANGE, onCheckBoxChange);
 	}
 
 	function setDeleteEvents() {
+		// 옵션 삭제 처리 - 확인필요
+		$('.optList').each(function(){
+			$(this).find('.js-opt-delete').click(function(e) {
+				e.preventDefault();
+				/*
+				eventManager.trigger(OPTIONNUM_EVENT.CHANGE, [$(this).parent(), 0]);
+				$(this).closest('.optList').remove();
+				*/
+			});
+		});
+
 		// 선택 리스트 삭제 처리
 		$(self.opts.listDel).on('click', function(e) {
 			e.preventDefault();
 
-			var list = $('[data-chk-group=\'myCartNewForm\']').not('[data-chk-role=\'chkAll\']').filter('.on'),
+			var list = $('[data-chk-group=\'myCartShop\']').not('[data-chk-role=\'chkAll\']').filter('.on'),
 			deleteList = [];
 
 			$.each(list, function() {
@@ -91,26 +120,108 @@ module.exports = function() {
 				return;
 			}
 
-			// myCartNewForm 리스트 삭제
+			// myCartShop 리스트 삭제
 			controller.deleteMyCartList(deleteList);
 		});
 	}
 
-	function onCheckBoxChange(/*e, target, chkGroup, chked*/) {
+	function onOptionNumChange(e, target, value) {
+		var info = target.closest(self.opts.cartList.wrap).data('list-info');
+		info.productQuantity = value;
+
+		target.closest(self.opts.cartList.wrap).attr('data-list-info', JSON.stringify(info));
+		displayUpdate();
+
+		controller.updateMyCartList(info.productNumber, {
+			"orderOptionNumber": target.data().optionNum,
+			"optionQuantity": value,
+			"productQuantity": value
+		});
+	}
+
+	function onCheckBoxChange(e, target, chkGroup, chked) {
+		var list = [];
+
+		$.each(chked, function() {
+			list.push($(this).closest(self.opts.cartList.wrap).get(0));
+		});
+
+		self.templatesWrap.find(self.opts.listSize).html(chked.size());
+
+		// 체크 수량에 따라 삭제 버튼 - 활성/비활성화 처리
+		if (chked.size()) {
+			self.templatesWrap.find(self.opts.listDel).removeAttr('disabled');
+		} else {
+			self.templatesWrap.find(self.opts.listDel).attr('disabled',true);
+		}
+
+		self.chkedList = list;
+
+		displayUpdate();
+	}
+
+	function displayUpdate() {
+		orderData = new Array();
+		
+		var list = (self.chkedList === null) ? self.templatesWrap.find(self.opts.cartList.wrap) : self.chkedList,
+		orderInfo = self.templatesWrap.find(self.opts.cartOrderInfo.wrap),
+		info = {},
+		value1 = 0,
+		value2 = 0,
+		totalBaseValue = 0,
+		discountValue = 0,
+		totalSaleValue = 0,
+		totalDeliveryValue = 0;
+
+		$.each(list, function() {
+			info = $(this).data('list-info');
+			value1 = (info.basePrice * info.productQuantity);// + info.deliveryCharge;
+			value2 = (info.salePrice * info.productQuantity) + info.deliveryCharge;
+
+			totalDeliveryValue += info.deliveryCharge;
+
+			totalBaseValue += value1;
+			discountValue += (info.basePrice-info.salePrice)*info.productQuantity;
+			totalSaleValue += value2;
+
+			$(this).find(self.opts.cartList.totalPrice).html(util.currencyFormat(value2));
+			$(this).find(self.opts.optionNum).html(info.productQuantity);
+
+			orderData.push({
+				"productNumber": info.productNumber,
+				"orderOptionNumber": info.optionNumber,
+				"productQuantity": info.productQuantity,
+				"optionQuantity": info.productQuantity,
+			});
+		});
+
+		orderInfo.find(self.opts.cartOrderInfo.price).html(util.currencyFormat(totalBaseValue));
+		orderInfo.find(self.opts.cartOrderInfo.discount).html(util.currencyFormat(discountValue));
+		orderInfo.find(self.opts.cartOrderInfo.delivery).html(util.currencyFormat(totalDeliveryValue));
+		orderInfo.find(self.opts.cartOrderInfo.totalPrice).html(util.currencyFormat(totalSaleValue));
+	}
+
+	function onCartOrderInfoToggler(e) {
+		e.preventDefault();
+
+		$(self.opts.cartOrderInfo.moreWrap).toggle();
 	}
 
 	function displayData(data) {
-		if (data.myCarts) {
-			$.map(data.myCarts, function(myCarts) {
-				myCarts.deliveryChargeDesc = util.currencyFormat(myCarts.deliveryCharge);
-				myCarts.productOrderPriceDesc = util.currencyFormat(myCarts.productOrderPrice);
-				myCarts.productSalePriceDesc = util.currencyFormat(myCarts.productSalePrice);
-			});
-		}
+		$.map(data, function(eachCartItem) {
+			if (eachCartItem.deliveryCharge == null) eachCartItem.deliveryCharge = 0;
+			eachCartItem.deliveryChargeDesc = util.currencyFormat(eachCartItem.deliveryCharge);
+			eachCartItem.basePriceDesc = util.currencyFormat(eachCartItem.basePrice);
+			eachCartItem.salePriceDesc = util.currencyFormat(eachCartItem.salePrice);
+
+			if (util.isLocal()) {
+				eachCartItem.productImageUrl = 'https://dev.koloncommon.com' + eachCartItem.productImageUrl;
+			}
+		});
 
 		var source = self.template.html(),
 		template = win.Handlebars.compile(source),
-		insertElements = $(template(data.myCarts));
+		insertElements = $(template(data));
 
 		self.templatesWrap.empty()
 							.addClass(self.opts.cssClass.isLoading)
@@ -122,7 +233,9 @@ module.exports = function() {
 								eventManager.triggerHandler(COLORBOX_EVENT.REFRESH);
 							});
 
-		self.templatesWrap.find(self.opts.listSize).html(self.templatesWrap.find(self.opts.newFormList.wrap).size());
+		self.templatesWrap.find(self.opts.listSize).html(self.templatesWrap.find(self.opts.cartList.wrap).size());
+
+		$(self.opts.cartOrderInfo.moreBtn).on('click', onCartOrderInfoToggler);
 	}
 
 	function onControllerListener(e, status, response) {
@@ -131,43 +244,25 @@ module.exports = function() {
 
 		switch(eventType) {
 			case CART_EVENT.LIST:
-				/*
-				401	Unauthorized
-				403	Forbidden
-				404	Not Found
-				 */
-				switch(status) {
-					case 200:
-						break;
-					default:
-						break;
-				}
-
 				debug.log(fileName, 'onControllerListener', eventType, status, response);
-				displayData(result.data);
+				displayData(result.data.myCarts);
+				displayUpdate();
 				setDeleteEvents();
+
+				$('#js-myCartShop-submit').click(function(e) {
+					e.preventDefault();
+
+					var loginData = loginDataModel.loginData();
+
+					if (loginData.stateCode == 'BM_MEM_STATE_01') {
+						$(document).trigger('verifyMember');
+					} else {
+						Cookies.set('instantOrder', orderData);
+						location.href = '/order/orderGoods.html';
+					}
+				});
 				break;
 			case CART_EVENT.DELETE:
-				/*
-				{
-					"status": "string",
-					"message": "string",
-					"errorCode": "string",
-					"data": {}
-				}
-				 */
-				/*
-				204	No Content
-				401	Unauthorized
-				403	Forbidden
-				 */
-				switch(status) {
-					case 200:
-						break;
-					default:
-						win.alert('HTTP Status Code ' + status);
-						break;
-				}
 				debug.log(fileName, 'onControllerListener', eventType, status, response);
 				testResult();
 				break;
@@ -175,7 +270,6 @@ module.exports = function() {
 	}
 
 	function testResult() {
-		win.alert('임시처리 결과처리 - location.reload');
 		win.location.reload();
 	}
 
