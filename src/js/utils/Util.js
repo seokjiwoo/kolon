1b602048a0a04e9fb4a8b3792e54e26227e567fa
hkroh <hkroh@vinyl-x.com>
2016-07-24 23:20:03 +0900

    [ Util.js ] isIE - ie 체크 메소드 추가

--- src/js/utils/Util.js
+++ src/js/utils/Util.js
@@ -125,7 +125,13 @@ function ClassUtils() {
 			 * 	레퍼러 체크 - 유효 레퍼러(kolocommon내 url) 일시 url 반환
 			 * @type {String}
 			 */
-			getReferrer : getReferrer
+			getReferrer : getReferrer,
+			/**
+			 * @description 
+			 * 	IE Detection
+			 * @type {Boolean}
+			 */
+			isIE : isIE
 		};
 	}
 
@@ -338,6 +344,11 @@ function ClassUtils() {
 		} else {
 			return '';
 		}
+	}
+
+	function isIE(userAgent) {
+		userAgent = userAgent || navigator.userAgent;
+		return userAgent.indexOf("MSIE ") > -1 || userAgent.indexOf("Trident/") > -1 || userAgent.indexOf("Edge/") > -1;
 	}
 
 }
