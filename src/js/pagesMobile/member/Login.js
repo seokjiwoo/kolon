9243c1c5482660797b6749f2633f8350233a8e62
hkroh <hkroh@vinyl-x.com>
2016-07-24 15:50:13 +0900

    [ Login.js ] 예외처리 코드 보정 / status > parseInt(status, 10);

--- src/js/pagesMobile/member/Login.js
+++ src/js/pagesMobile/member/Login.js
@@ -192,6 +192,8 @@ module.exports = function() {
 	function loginCompleteHandler(e, status, response) {
 		var keepLogin = $('#saveInfoBox').hasClass('on') ? 'Y' : 'N';
 
+		status = parseInt(status, 10);
+
 		switch(status) {
 			case 200:
 			case 201:
@@ -256,6 +258,9 @@ module.exports = function() {
 							]
 						);
 						controller.notarobot();
+						break;
+					default:
+						Super.Super.alertPopup('로그인/회원가입에 실패하였습니다.', response.message, '확인');
 						break;
 				}
 				break;
