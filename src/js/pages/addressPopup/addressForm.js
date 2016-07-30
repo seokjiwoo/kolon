363285a3e8c3b4bb338635e48547baeaa6aeac39
kimhyoseok <toriworks@gmail.com>
2016-07-30 15:55:31 +0900

    주소 별칭 입력 오류 수정

--- src/js/pages/addressPopup/addressForm.js
+++ src/js/pages/addressPopup/addressForm.js
@@ -88,10 +88,11 @@ module.exports = function() {
 		var adrName1Val = $.trim($('#adrName1').val());
 		var adrName2Val = $.trim($('#adrName2').val());
 
-		if (adrName1Val == '' || true == adrName1Val.contains('주소 별칭을 입력해 주세요')) {
+
+		if (adrName1Val == '' || adrName1Val.indexOf('주소의 별칭을 입력해주세요') != -1) {
 			alert('주소 별칭을 입력해 주세요');
 			return;
-		} else if (adrName2Val == '' || true == adrName2Val.contains('이름을 입력해 주세요')) {
+		} else if (adrName2Val == '' || adrName2Val.indexOf('이름을 입력해주세요') != -1) {
 			alert('이름을 입력해 주세요');
 			return;
 		} else if ($.trim($('#phone2').val()) == '' || $.trim($('#phone3').val()) == '') {
